d26123a87aea1755107e57a0b3325d6d334d629e
TimiDavido <159925029+TimiDavido@users.noreply.github.com>
2024-02-29 14:50:59 +0100

    class

---
+++ class.js
@@ -0,0 +1,38 @@
+// class product {
+//     constructor(name,price){
+//         this.name = name,
+//         this.price = price
+//     }
+
+//     displayProduct(){
+//         console.log("The name of the product is " + this.name)
+//         console.log("The price is $" + this.price);
+//     }
+// }
+
+// const product1 = new product ("BlackBerry", 100)
+// const product2 = new product ("Iphone 4", 200)
+
+// product1.displayProduct()
+
+
+
+
+// class user {
+//     static usercount = 0;
+    
+//     constructor(name){
+//         this.name = name
+
+//         user.usercount++
+//     }
+// }
+// const user1 = new user ("Timi")
+// const user2 = new user ("Ade")
+
+// console.log(user1.name)
+
+
+
+
+
