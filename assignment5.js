7658ce6bb0a7f531b776f422eb1231f82e838ab5
TimiDavido <159925029+TimiDavido@users.noreply.github.com>
2024-03-13 09:45:33 +0100

    Assignment 5

---
+++ assignment5.js
@@ -0,0 +1,35 @@
+//Given a month as an integer number from 1-12 return to which quater it belongs too as an integer number
+// function monthAsQuater(month){
+//     switch(month){
+//         case 1 :
+//         case 2 :
+//         case 3 :
+//             return 1
+//         case 4 :
+//         case 5 :
+//         case 6 :
+//             return 2
+//         case 7 :
+//         case 8 :
+//         case 9 :
+//             return 3
+//         case 10 :
+//         case 11 :
+//         case 12 :
+//              return 4
+//             default:
+//         return 0;
+//     }
+// }
+// monthAsQuater(2)
+
+  // function monthAsQuarter(month){
+  // return month > 0 && month < 4 ? 1 :  
+  //        month > 3 && month < 7 ? 2 : 
+  //        month > 6 && month < 10 ? 3 :
+  //        month > 9 && month < 13 ? 4 :
+  //                                  0;
+  // }
+  // monthAsQuarter(13)
+  
+
